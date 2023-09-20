7f9f20cedd6a414259c3d6d8e90c9da1dbaf853a
Aleksander <asava@sibmail.com>
2023-09-20 23:35:13 +0700

    Авторизация

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -1,12 +1,29 @@
+import { AuthDto } from './dto/auth.dto'
 import { AuthService } from './auth.service'
-import { Body, Controller, Post } from '@nestjs/common'
+import {
+	Body,
+	Controller,
+	HttpCode,
+	Post,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common'
 
 @Controller('auth')
 export class AuthController {
-  // /auth/register
-  constructor(private readonly AuthService: AuthService) {}
-  @Post('register')
-  async register(@Body() dto: any) {
-    return this.AuthService.register(dto)
-  }
+	constructor(private readonly AuthService: AuthService) {}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('login')
+	async login(@Body() dto: AuthDto) {
+		return this.AuthService.login(dto)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('register')
+	async register(@Body() dto: AuthDto) {
+		return this.AuthService.register(dto)
+	}
 }
--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -1,25 +1,48 @@
-import { Injectable } from '@nestjs/common'
+import {
+	BadRequestException,
+	Injectable,
+	UnauthorizedException,
+} from '@nestjs/common'
 import { ModelType } from '@typegoose/typegoose/lib/types'
 import { InjectModel } from 'nestjs-typegoose'
+import { hash, genSalt, compare } from 'bcryptjs'
+
+import { AuthDto } from './dto/auth.dto'
 import { UserModel } from 'src/user/user.model'
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
-  ) {}
-  async register(dto: any) {
-    const newUser = new this.UserModel(dto)
-    return newUser.save()
-  }
+	constructor(
+		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
+	) {}
+
+	async login(dto: AuthDto) {
+		return this.validateUser(dto)
+	}
+
+	async register(dto: AuthDto) {
+		const oldUser = await this.UserModel.findOne({ email: dto.email })
+		if (oldUser) {
+			throw new BadRequestException('Юзер с таким email есть уже в системе')
+		}
+		const salt = await genSalt(10)
+		const newUser = new this.UserModel({
+			email: dto.email,
+			password: await hash(dto.password, salt),
+		})
+		return newUser.save()
+	}
+
+	async validateUser(dto: AuthDto): Promise<UserModel> {
+		const user = await this.UserModel.findOne({ email: dto.email })
+		if (!user) {
+			throw new UnauthorizedException('Юзер с таким email нет в системе')
+		}
+
+		const isValidPassword = await compare(dto.password, user.password)
+		if (!isValidPassword) {
+			throw new UnauthorizedException('Не верный пароль')
+		}
+		return user
+	}
 }
-
-// import { Injectable } from '@nestjs/common'
-// import { ModelType } from '@typegoose/typegoose/lib/types'
-// import { InjectModel } from 'nestjs-typegoose'
-
-// @Injectable()
-// export class AuthService {
-//   constructor() {} // @InjectModel(UserModel) private readonly Usermodel: ModelType<UserModel>
-//   async register(dto: any) {}
-// }
