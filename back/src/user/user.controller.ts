4a8aa9bc2dc7e799547ff5fe1b8cdf5742c265ff
Aleksander <asava@sibmail.com>
2023-09-22 23:38:15 +0700

    Guard и декораторы

--- back/src/user/user.controller.ts
+++ back/src/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common'
+import { Auth } from 'src/auth/decorators/auth.decorator'
 import { UserService } from './user.service'
-
+// 18:23
 @Controller('users')
 export class UserController {
 	constructor(private readonly userService: UserService) {}
 
 	@Get('profile')
-	// @Auth()
+	@Auth('admin')
 	async getProfile() {
 		return this.userService.byId()
 	}
