67d7e4af025e699f27d7c20a642f0d7459e67bce
Aleksander <asava@sibmail.com>
2023-09-23 19:03:58 +0700

    Guard и декораторы

--- back/src/user/user.controller.ts
+++ back/src/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common'
 import { Auth } from 'src/auth/decorators/auth.decorator'
+import { User } from './decorators/user.decorator'
 import { UserService } from './user.service'
 // 18:23
 @Controller('users')
@@ -7,8 +8,9 @@ export class UserController {
 	constructor(private readonly userService: UserService) {}
 
 	@Get('profile')
-	@Auth('admin')
-	async getProfile() {
-		return this.userService.byId()
+	// @Auth('admin')
+	@Auth()
+	async getProfile(@User('_id') _id: string) {
+		return this.userService.byId(_id)
 	}
 }
--- back/src/user/user.service.ts
+++ back/src/user/user.service.ts
@@ -1,14 +1,18 @@
 import { UserModel } from 'src/user/user.model'
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { InjectModel } from 'nestjs-typegoose'
 import { ModelType } from '@typegoose/typegoose/lib/types'
 
 @Injectable()
 export class UserService {
 	constructor(
-		@InjectModel(UserModel) private readonly userModel: ModelType<UserModel>
+		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
 	) {}
-	async byId() {
-		return { email: 'tr@tu.ru' }
+	async byId(_id: string) {
+		const user = await this.UserModel.findById(_id)
+		if (!user) {
+			throw new NotFoundException('Юзер не найден')
+		}
+		return user
 	}
 }
