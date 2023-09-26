71c0cde7a8d1ebe7434c63f340a8ecc21e1590e4
Aleksander <asava@sibmail.com>
2023-09-26 22:07:45 +0700

    ok

--- back/src/files/file.service.ts
+++ back/src/files/file.service.ts
@@ -26,25 +26,3 @@ export class FilesService {
 		return res
 	}
 }
-
-// @Injectable()
-// export class FilesService {
-// 	async saveFiles(
-// 		files: Express.Multer.File[],
-// 		folder: string = 'default'
-// 	): Promise<FileResponse[]> {
-// 		const uploadFolder = `${path}/uploads/${folder}`
-// 		await ensureDir(uploadFolder)
-// 		const res: FileResponse[] = await Promise.all(
-// 			files.map(async (file) => {
-// 				await writeFile(`${uploadFolder}/${file.originalname}`, file.buffer)
-// 				return {
-// 					url: `/uploads/${folder}/${file.originalname}`,
-// 					name: file.originalname,
-// 				}
-// 			})
-// 		)
-
-// 		return res
-// 	}
-// }
