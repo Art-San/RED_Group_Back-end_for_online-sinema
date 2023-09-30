b4e5aad14a9144a3b18d6a237313436581782ea5
Aleksander <asava@sibmail.com>
2023-09-30 20:42:19 +0700

    16. Телеграм уведомления

--- back/src/telegram/telegram.service.ts
+++ back/src/telegram/telegram.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { getTelegramConfig } from './../config/telegram.config'
+import { Injectable } from '@nestjs/common'
+import { Telegraf } from 'telegraf'
+import { Telegram } from './telegram.interface'
 
 @Injectable()
-export class TelegramService {}
+export class TelegramService {
+	bot: Telegraf
+	options: Telegram
+
+	constructor() {
+		this.options = getTelegramConfig()
+		this.bot = new Telegraf(this.options.token)
+	}
+}
